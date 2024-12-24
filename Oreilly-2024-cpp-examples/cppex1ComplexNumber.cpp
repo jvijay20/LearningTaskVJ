9f4997b8c6859f4ea556bded926da797a84cdd49
V J <vijayabharathi20@gmail.com>
2024-12-24 12:14:59 +0100

    202424: Cpp ex1
    - create complex number class

---
+++ Oreilly-2024-cpp-examples/cppex1ComplexNumber.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+class ComplexNumber
+{
+    private:
+    float realPart, complexPart;
+    public:
+    ComplexNumber()
+    {
+        std::cout << "No arg-constructor called"<< std::endl;
+    }
+    void setMemberVariables(float r, float c)
+    {
+        realPart = r;
+        complexPart = c;
+    }
+    float getRealPart()
+    {
+        return realPart;
+    }
+    float getComplexPart()
+    {
+        return complexPart;
+    }
+    void print()
+    {
+        std::cout <<"real = " << realPart << " complex " << complexPart << std::endl;
+    }
+};
+
+int main()
+{
+    ComplexNumber c, d;
+    c.setMemberVariables(5, 3);
+    std::cout << "Complex part = " << c.getComplexPart() << std::endl;
+    c.print();
+    d.setMemberVariables(5.3, 3.5);
+    std::cout << "Complex part = " << d.getComplexPart() << std::endl;
+    d.print();
+    return 0;
+}
